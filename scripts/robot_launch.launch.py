a6fc81c0c146509909667cb8ff9e7590c094d49b
dkanou <dkanoulas@gmail.com>
2025-01-30 11:37:08 +0000

    fixes

--- scripts/robot_launch.launch.py
+++ scripts/robot_launch.launch.py
@@ -1,11 +1,22 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, ExecuteProcess, GroupAction
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument, GroupAction, TimerAction
+from launch.substitutions import LaunchConfiguration
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
 import os
 
 def generate_launch_description():
+    # Declare the use_sim_time argument
+    declare_use_sim_time = DeclareLaunchArgument(
+        'use_sim_time',
+        default_value='true',  # Default to true for simulation time
+        description='Use simulation (Gazebo) clock if true'
+    )
+
+    # Get the use_sim_time configuration to pass to nodes and included launch files
+    use_sim_time = LaunchConfiguration("use_sim_time")
+
     # ros2 launch go2_config gazebo_mid360.launch.py
     gazebo_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
@@ -15,7 +26,7 @@ def generate_launch_description():
                 'gazebo_mid360.launch.py'
             )
         ),
-        launch_arguments={'rviz': 'true'}.items()
+        launch_arguments={'use_sim_time': use_sim_time}.items()  # Pass sim_time
     )
 
     # ros2 launch fast_lio mapping.launch.py
@@ -26,54 +37,56 @@ def generate_launch_description():
             'mapping.launch.py'
             )
         ),
-        launch_arguments={'config_file': 'unitree_go2_mid360.yaml'}.items()
+        launch_arguments={'config_file': 'unitree_go2_mid360.yaml', 'use_sim_time': use_sim_time}.items()
     )
 
     # ros2 run waypoint_follower waypoint_follower
-    waypoint_follower_node = ExecuteProcess(
-        cmd=['ros2', 'run', 'waypoint_follower', 'waypoint_follower'],
-        output='screen'
+    waypoint_follower_node = Node(
+        package='waypoint_follower',
+        executable='waypoint_follower',
+        output='screen',
+        parameters=[{"use_sim_time": use_sim_time}]
     )
     
     # ros2 run local_map_creator local_map_creator
-    local_map_node = ExecuteProcess(
-        cmd=['ros2', 'run', 'local_map_creator', 'local_map_creator'],
-        output='screen'
+    local_map_node = Node(
+        package='local_map_creator',
+        executable='local_map_creator',
+        output='screen',
+        parameters=[{"use_sim_time": use_sim_time}]
     )
 
     # ros2 run static_tf_node to connect the robot with the camera
     static_tf_node = Node(
         package='tf2_ros',
         executable='static_transform_publisher',
-        arguments=['0', '0', '0', '1.57', '0', '0', 'body', 'odom'],
-        output='screen'
+        arguments=['0', '0', '0', '1.57', '0', '0', 'odom', 'body'],
+        output='screen',
+        parameters=[{"use_sim_time": use_sim_time}]
     )
 
     # segment groups
     gazebo_group = GroupAction([
         gazebo_launch
     ])
-
     mapping_group = GroupAction([
         mapping_launch
     ])
-
-    waypoint_follower_group = GroupAction([
-        waypoint_follower_node
-    ])
-    
     local_map_group = GroupAction([
         local_map_node
     ])
-
     static_tf_group = GroupAction([
         static_tf_node
     ])
+    waypoint_follower_group = GroupAction([
+        waypoint_follower_node
+    ])
 
     return LaunchDescription([
+        declare_use_sim_time,  # Declare use_sim_time
         gazebo_group,
         mapping_group,
-        waypoint_follower_group,
         local_map_group,
-        static_tf_group
+        static_tf_group,
+        waypoint_follower_group
     ])
